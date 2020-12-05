297a7ecffc1124c1d78b30b2021fbd50a5fb2d95
Bronson <58447783+BronsonN@users.noreply.github.com>
2020-12-04 19:35:53 -0700

    Add files via upload

---
+++ CarTest.java
@@ -0,0 +1,14 @@
+
+public class carTest {
+
+  public static void main(String[] args) {
+
+    Car c1 = new Car("Honda");
+    Car c2 = new Car("Jeep");
+
+    System.out.println("Car ones brand is: " +c1.getbrand());
+    System.out.println("Car twos brand is: " +c2.getbrand());
+    System.out.println("Car twos age is: " +c2.getYear());
+  }
+
+}
